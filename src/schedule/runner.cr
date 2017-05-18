e952103470c656f0ff7c83da2178b874ee9ed788
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-05-18 11:19:37 -0300

    feature: Add Schedule::Runner to isolate tasks

--- src/schedule/runner.cr
+++ src/schedule/runner.cr
@@ -1,4 +1,37 @@
 module Schedule
   class Runner
+    @exception_handler : (Proc(Nil) | Proc(Exception))?
+
+    def every(interval, &block)
+      spawn do
+        loop do
+          begin
+            sleep interval
+            block.call
+          rescue ex : StopException
+            break
+          rescue ex : RetryException
+            next
+          rescue ex
+            if handler = @exception_handler
+              begin
+                case h = handler
+                when Proc(Nil)
+                  h.call
+                when Proc(Exception, Nil)
+                  h.call(ex)
+                end
+              rescue ex : StopException
+                break
+              end
+            end
+          end
+        end
+      end
+    end
+
+    def exception_handler(&block)
+      @exception_handler = block
+    end
   end
 end
