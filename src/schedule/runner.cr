3c4d6c71aa04b6094edf0a380b2a3e2152f62b4a
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-09-22 14:49:15 -0300

    refact: Extract running logic into a private macro

--- src/schedule/runner.cr
+++ src/schedule/runner.cr
@@ -5,27 +5,8 @@ module Schedule
     def every(interval, &block)
       spawn do
         loop do
-          begin
-            sleep interval
-            block.call
-          rescue ex : StopException
-            break
-          rescue ex : RetryException
-            next
-          rescue ex
-            if handler = @exception_handler
-              begin
-                case h = handler
-                when Proc(Nil)
-                  h.call
-                when Proc(Exception, Nil)
-                  h.call(ex)
-                end
-              rescue ex : StopException
-                break
-              end
-            end
-          end
+          sleep interval
+          run(block)
         end
       end
     end
@@ -33,27 +14,30 @@ module Schedule
     def after(interval, &block)
       delay(interval) do
         loop do
+          run(block)
+          break
+        end
+      end
+    end
+
+    private macro run(block)
+      begin
+        block.call
+      rescue ex : StopException
+        break
+      rescue ex : RetryException
+        next
+      rescue ex
+        if handler = @exception_handler
           begin
-            block.call
-            break
+            case h = handler
+            when Proc(Nil)
+              h.call
+            when Proc(Exception, Nil)
+              h.call(ex)
+            end
           rescue ex : StopException
             break
-          rescue ex : RetryException
-            next
-          rescue ex
-            if handler = @exception_handler
-              begin
-                case h = handler
-                when Proc(Nil)
-                  h.call
-                when Proc(Exception, Nil)
-                  h.call(ex)
-                end
-              rescue ex : StopException
-                break
-              end
-              break
-            end
           end
         end
       end
